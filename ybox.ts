eaa02bd8320e2deb0592f24617f6c5d58094c815
Rémi Emonet <remi.emonet@reverse--com.heeere>
2026-02-22 22:27:43 +0100

    Pending code that allows creation

--- ybox.ts
+++ ybox.ts
@@ -1,4 +1,5 @@
 import * as Y from 'yjs'
+import type { YAny } from './ytools'
 
 export const SEP = '::'
 export const EG_CONFIG_NOBOX = `todo.com${SEP}doc${SEP}token${SEP}sub/folder`
@@ -48,9 +49,6 @@ export function getYBoxFor(doc: Y.Doc, path: string): Y.Map<any> | undefined {
 }
 
 export function getYTextInBox(box: Y.Map<unknown>, path: string, create: boolean = false): Y.Text {
-  if (create) {
-    throw new Error('Not implemented')
-  }
   const steps = path.split('/').filter((s) => s)
   if (steps.length === 0) throw new Error('Path cannot be empty')
   if (!box.has(ROOT_ID)) {
@@ -58,7 +56,8 @@ export function getYTextInBox(box: Y.Map<unknown>, path: string, create: boolean
   }
   const root = box.get(ROOT_ID)
   let p = root as Y.Map<unknown>
-  while (steps.length > 0) {
+  let pid = ROOT_ID
+  while (steps.length > (create ? 1 : 0)) {
     const k = steps.shift()!
     if (p.get('type') !== 'directory') {
       throw new Error(`Path ${path} is not a directory (at ${k})`)
@@ -69,9 +68,42 @@ export function getYTextInBox(box: Y.Map<unknown>, path: string, create: boolean
       throw new Error(`Path ${path} does not exist (at ${k})`)
     }
     p = box.get(subk) as Y.Map<unknown>
+    pid = subk
   }
-  if (p.get('type') !== 'text') {
-    throw new Error(`Path ${path} is not a text`)
+  if (create) {
+    const k = steps.shift()!
+    if (p.get('type') !== 'directory') {
+      throw new Error(`(create) Last path ${path} is not a directory (at ${k})`)
+    }
+    const children = p.get('children') as Y.Map<string>
+    const subk = children.get(k)
+    if (!subk) {
+      const newId = _generateId()
+      if (children.has(newId)) {
+        throw new Error(`Generated id ${newId} already exists in children, retry`) // very unlikely
+      }
+      const newTextFile = new Y.Map<YAny>()
+      newTextFile.set('type', 'text')
+      newTextFile.set('parent', pid)
+      newTextFile.set('ctime', Date.now())
+      newTextFile.set('mtime', Date.now())
+      const content = new Y.Text()
+      newTextFile.set('content', content)
+      children.set(k, newId)
+      box.set(newId, newTextFile)
+      return content
+    } else {
+      throw new Error(`Path ${path} already exists (at ${k})`)
+    }
+  } else {
+    if (p.get('type') !== 'text') {
+      throw new Error(`Path ${path} is not a text`)
+    }
+    return p.get('content') as Y.Text
   }
-  return p.get('content') as Y.Text
+}
+
+export function _generateId(): string {
+  return "id:" + Math.random().toString(36).slice(2, 10);
+  // or: return crypto.randomUUID();
 }
